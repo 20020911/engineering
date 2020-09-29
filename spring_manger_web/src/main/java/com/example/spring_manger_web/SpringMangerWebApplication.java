f53cc39736f8b3cff0a2382cc7ce63cf7741af34
2891792857 <2891792857@qq.com>
2020-09-29 10:53:30 +0800

    12sas

---
+++ spring_manger_web/src/main/java/com/example/spring_manger_web/SpringMangerWebApplication.java
@@ -0,0 +1,15 @@
+package com.example.spring_manger_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SpringBootApplication
+public class SpringMangerWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMangerWebApplication.class, args);
+    }
+
+}
---
+++ spring_manger_web/src/test/java/com/example/spring_manger_web/SpringMangerWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_manger_web;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMangerWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
